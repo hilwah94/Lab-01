985c4944f686fdd85937145ed86bb76136e4e24b
hilwa94 <helwaomar@gmail.com>
2021-04-15 15:53:25 +0300

    finish lab01

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Lab-01
+# Lab-01b
+**_Class 1 Lab_**
+
+## done by :-  
+**omar hilwah**
+
+> **_ in the java script we did four questios to interact with the user evrey one with variable and the value for each one is separetly consoled, replayed every question with kind reply like we did in the class, and a final messege with the four prompt_**
+### Links & Resources :- 
+
+[w3schools](https://www.w3schools.com/)
+[Stackoverflow](https://www.stackoverflow.com)
+[github](https://www.githib.com)
+[Canvas](https://canvas.instructure.com/)
+[Youtube](https://www.youtube.com/)
+
+
+
---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+'use strict';
+
+// four questios to interact with the user 
+// evrey one with variable and the value for each one is 
+// separetly consoled 
+// replayed every question with kind reply like
+// we did in the class
+// final messege with the four prompt
+
+alert('welcome to my web page');
+
+let userName = prompt('What\'s your name ?')
+console.log('user name is ' + userName)
+alert('Nice Name ' + userName)
+
+let userAge = prompt('What\'s your age ?')
+console.log('user age is ' + userAge)
+alert('God bless you ' + userName)
+
+let userFavFood = prompt('What\'s your Favourite food ?')
+console.log('user Favourite food is ' + userFavFood)
+alert('eat ' + userFavFood + ' until you can\'t no more lol')
+
+let userHoppie = prompt(' How do you like to spend your time ?')
+console.log('user hoppie is ' + userHoppie)
+alert('work hard but remember to give time to ' + userHoppie)
+
+alert('welcome ' + userName +
+ ' your age is ' + userAge +' and we wish you a healthy life ' +
+ ' you seems to like eating ' + userFavFood +
+ ' and nice hoppy you have, ' + userHoppie + ' is a great way to spend time ')
